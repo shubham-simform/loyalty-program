e51e829c0590ffba32abc6fed3acca9cef6aaa76
Shubham Rajput <shubham.r@simformsolutions.com>
2022-07-28 14:54:03 +0530

    Added devise view pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :validatable
 
   #validations
   validates :first_name, :last_name, :date_of_birth, :country, presence: true
