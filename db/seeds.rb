6b1ed69716c881ecc54139c8b630aca4c65d3bed
Shubham Rajput <shubham.r@simformsolutions.com>
2022-07-28 19:49:31 +0530

    Fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,9 @@
-# frozen_string_literal: true
-
 require 'faker'
 include FactoryBot::Syntax::Methods
 
-# Creating test user used for login
-user = User.find_or_create_by(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: 'test@gmail.com', password: 'Simform@123',
+user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: 'test@gmail.com', password: 'Simform@123',
                    date_of_birth: Faker::Date.in_date_period, country: 'India')
 
-# Creating basic Rewards which can be used in the applications
 rewards = [{ name: '5% Cash Rebate' },
            { name: 'Free Coffee_Reward' },
            { name: 'Free Movie ticket' },
@@ -15,10 +11,8 @@ rewards = [{ name: '5% Cash Rebate' },
 
 Reward.create(rewards)
 
-# Creating 20 test users
 create_list :user, 10, :default_country
 
 create_list :user, 10, :other_country
 
-# Creating 10 transactions for the users
-create_list :transaction, 10, user_id: user.id
+create_list :transaction, 10, user: User.last, country: 'India'
