ae26d446ce23a4850dd63c50f4c3f6207e2df071
Shubham Rajput <shubham.r@simformsolutions.com>
2022-07-28 18:24:19 +0530

    Fixed user sign up issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,7 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
+  def after_sign_up_path_for(resource)
+    root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :users
   resources :transactions, only: %i[new create index]
 
   root 'transactions#index'
